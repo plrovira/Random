9bdcc63f9e34087970ff070288efa8fc9835c3b9
plrovira <pll.rovira@edn.cat>
2015-07-04 19:22:12 +0200

    Versión 2
    Optimización del codigo javascript de ramdom

--- random.js
+++ random.js
@@ -1,8 +1,8 @@
 var numero = Math.random();
 
+var str=' MAYOR que 0,5';
 if (numero <= 0.5){
-	console.log("\n"+numero + 'menor que 0,5 \n ');
+	str = ' MENOR que 0,5';
 	
-}else{
-	console.log('\n' + numero + ' MAYOR que 0,5 \n');
 }
+console.log('\n' + numero + str + '\n');
